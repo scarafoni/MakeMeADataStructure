c077dd18f9971220352e0d4deab4e396bc2363b0
Dan Scarafoni <dan@scarafoni.com>
2014-12-30 15:44:19 -0800

    print to puts

--- make_me_a_data_structure.rb
+++ make_me_a_data_structure.rb
@@ -7,15 +7,14 @@ start with stock classes
 pick one based on what they want for access time
 =end
 
-if __FILE__ == $0
-  puts "Is the order you put the data in important? [y/n]"
-case gets.chomp
-when 'y'
-  print 'y'
-when 'n'
-  print 'n'
-else
-  print "fail"
-
-  print order_important
+if __FILE__ == $PROGRAM_NAME
+  puts 'Is the order you put the data in important? [y/n]'
+  case gets.chomp
+  when 'y'
+    puts 'y'
+  when 'n'
+    puts 'n'
+  else
+    puts 'fail'
+  end
 end
