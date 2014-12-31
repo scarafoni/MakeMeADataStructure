4b83fb80a0f40515ab309550612909c3f2067491
Dan Scarafoni <dan@scarafoni.com>
2014-12-31 01:50:46 -0800

    hashtable writer

--- make_me_a_data_structure.rb
+++ make_me_a_data_structure.rb
@@ -57,14 +57,14 @@ if __FILE__ == $PROGRAM_NAME
     file_text = 'class Node\n'
     file_text << fields.map { |x| '  attr_accessor :' + x }.join('\n') + '\n'
     file_text << '  def initialize(next, ' + fields.join(', ') + ')\n'
-    file_text << fields.map { |x| '    @' + x }.join('\n')
+    file_text << fields.map { |x| '    @' + x }.join('\n') + '\n'
     file_text << '  end \n\n'
     file_text << 'end \n\n'
     # stack class
     file_text << 'class Stack\n'
     file_text << '  attr_accessor :head\n'
     file_text << '  def initialize()\n'
-    file_text << '    head = Node.new("None"' + fields.join(', ') + '\n'
+    file_text << '    head = Node.new("None"' + fields.join(', ') + ')\n'
     file_text << '  end\n'
     file_text << '  \n'
     file_text << '  def push(newNode)\n'
@@ -81,24 +81,22 @@ if __FILE__ == $PROGRAM_NAME
     file_text = 'class Node\n'
     file_text << fields.map { |x| 'attr_accessor :' + x }.join('\n')
     file_text << '\n  def initialize(' + fields.join(', ') + ')\n'
+    file_text << fields.map { |x| '    @' + x }.join('\n') + '\n'
+    file_text << '  end \n\n'
     file_text << 'end \n\n'
-    # stack class
-    file_text << 'class Stack\n'
-    file_text << '  attr_accessor :head\n'
+    # hashtable class
+    file_text << 'class HashTable\n'
     file_text << '  def initialize()\n'
-    file_text << '    head = Node.new(' + fields.join(', ') + '\n'
+    file_text << '    @table = Array.new(1000)\n'
     file_text << '  end\n'
     file_text << '  \n'
-    file_text << '  def push(newNode)\n'
-    file_text << '    newNode.next = @head\n'
-    file_text << '    @head=newNode\n'
+    file_text << '  def add(new_node)\n'
+    file_text << '    table \n'
+    file_text << '    @table[int(new_node) % 1000] = newNode\n'
     file_text << '  end\n'
-    file_text << '  def pop()\n'
-    file_text << '    temp = @head.next\n'
-    file_text << '    @head.next = @head.next.next\n'
-    file_text << '    return temp\n'
+    file_text << '  def get(node)\n'
+    file_text << '    return @table[int(node) % 1000]\n'
     file_text << '  end\n'
     file_text << 'end\n\n'
-    # hash_table
   end
 end
