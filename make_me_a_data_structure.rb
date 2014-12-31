f99d78c36349d89a9186ccde30f9ed28dfc33114
Dan Scarafoni <dan@scarafoni.com>
2014-12-30 16:12:19 -0800

    working io

--- make_me_a_data_structure.rb
+++ make_me_a_data_structure.rb
@@ -7,14 +7,30 @@ start with stock classes
 pick one based on what they want for access time
 =end
 
+# simple IO function(s)
+module IIO
+  # loops until a valid input comes
+  def get_valid_input(inputs)
+    loop do
+      input = gets.chomp
+      if inputs.include? input
+        return input
+      else
+        puts 'error, invalid input'
+      end
+    end
+  end
+end
+
 if __FILE__ == $PROGRAM_NAME
+  include IIO
   puts 'Is the order you put the data in important? [y/n]'
-  case gets.chomp
+  case IO.get_valid_input(%w(y n))
   when 'y'
     puts 'y'
   when 'n'
     puts 'n'
   else
-    puts 'fail'
+    puts 'UNKNOWN ERROR'
   end
 end
