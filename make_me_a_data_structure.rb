95ef0f745cef469f0d8cdda4a3a1c438f6716b76
Dan Scarafoni <dan@scarafoni.com>
2015-01-02 00:57:39 -0500

    fixed problem with newlines

--- make_me_a_data_structure.rb
+++ make_me_a_data_structure.rb
@@ -14,21 +14,22 @@ module MyIO
     loop do
       puts prompt
       input = gets.chomp
-      if inputs.include? input return input
+      if inputs.include? input
+        return input
       else
-        puts 'error, invalid input'
+        puts "error, invalid input"
       end
     end
   end
-  
+
   # specifically for yes no input
   def get_yn_input(prompt)
     loop do
       puts prompt
       case gets.chomp
-      when 'y'
+      when "y"
         return true
-      when 'n'
+      when "n"
         return false
       end
     end
@@ -39,64 +40,67 @@ if __FILE__ == $PROGRAM_NAME
   include MyIO
 
   # is order important?
-  prompt = 'Is the order you put the data in important? [y/n]'
-  order = MyIO.get_yn_input(prompt, %w(y n))
+  prompt = "Is the order you put the data in important? [y/n]"
+  order = MyIO.get_yn_input(prompt)
 
   # is it important to have constant lookup?
-  prompt = 'do you need it to have constant lookup time?'
-  lookup = MyIO.get_yn_input(prompt, %w(y n))
+  prompt = "do you need it to have constant lookup time?"
+  lookup = MyIO.get_yn_input(prompt)
 
-  # get the data we're gonna store
-  puts 'Enter, separated by spaces, the fields you want to store at each node.'
-  fields = gets.chomp.split(' ')
+  # get the data we"re gonna store
+  puts "Enter, separated by spaces, the fields you want to store at each node."
+  fields = gets.chomp.split(" ")
 
-  ds_file = open('ds.rb', 'w')
+  ds_file = open("ds.rb", "w")
   # now actually generate the needed data structure
   if order
     # node class
-    file_text = 'class Node\n'
-    file_text << fields.map { |x| '  attr_accessor :' + x }.join('\n') + '\n'
-    file_text << '  def initialize(next, ' + fields.join(', ') + ')\n'
-    file_text << fields.map { |x| '    @' + x }.join('\n') + '\n'
-    file_text << '  end \n\n'
-    file_text << 'end \n\n'
+    file_text = "class Node\n"
+    file_text << fields.map { |x| "  attr_accessor :" + x }.join("\n") + "\n"
+    file_text << "  def initialize(next, " + fields.join(", ") + ")\n"
+    file_text << fields.map { |x| "    @" + x }.join("\n") + "\n"
+    file_text << "  end \n\n"
+    file_text << "end \n\n"
     # stack class
-    file_text << 'class Stack\n'
-    file_text << '  attr_accessor :head\n'
-    file_text << '  def initialize()\n'
-    file_text << '    head = Node.new("None"' + fields.join(', ') + ')\n'
-    file_text << '  end\n'
-    file_text << '  \n'
-    file_text << '  def push(newNode)\n'
-    file_text << '    newNode.next = @head\n'
-    file_text << '    @head=newNode\n'
-    file_text << '  end\n'
-    file_text << '  def pop()\n'
-    file_text << '    temp = @head.next\n'
-    file_text << '    @head.next = @head.next.next\n'
-    file_text << '    return temp\n'
-    file_text << '  end\n'
-    file_text << 'end\n\n'
+    file_text << "class Stack\n"
+    file_text << "  attr_accessor :head\n"
+    file_text << "  def initialize()\n"
+    file_text << "    head = Node.new('None'" + fields.join(", ") + ")\n"
+    file_text << "  end\n"
+    file_text << "  \n"
+    file_text << "  def push(newNode)\n"
+    file_text << "    newNode.next = @head\n"
+    file_text << "    @head=newNode\n"
+    file_text << "  end\n"
+    file_text << "  def pop()\n"
+    file_text << "    temp = @head.next\n"
+    file_text << "    @head.next = @head.next.next\n"
+    file_text << "    return temp\n"
+    file_text << "  end\n"
+    file_text << "end\n\n"
   else
-    file_text = 'class Node\n'
-    file_text << fields.map { |x| 'attr_accessor :' + x }.join('\n')
-    file_text << '\n  def initialize(' + fields.join(', ') + ')\n'
-    file_text << fields.map { |x| '    @' + x }.join('\n') + '\n'
-    file_text << '  end \n\n'
-    file_text << 'end \n\n'
+    file_text = "class Node\n"
+    file_text << fields.map { |x| "attr_accessor :" + x }.join("\n")
+    file_text << "\n  def initialize(" + fields.join(", ") + ")\n"
+    file_text << fields.map { |x| "    @" + x }.join("\n") + "\n"
+    file_text << "  end \n\n"
+    file_text << "end \n\n"
     # hashtable class
-    file_text << 'class HashTable\n'
-    file_text << '  def initialize()\n'
-    file_text << '    @table = Array.new(1000)\n'
-    file_text << '  end\n'
-    file_text << '  \n'
-    file_text << '  def add(new_node)\n'
-    file_text << '    table \n'
-    file_text << '    @table[int(new_node) % 1000] = newNode\n'
-    file_text << '  end\n'
-    file_text << '  def get(node)\n'
-    file_text << '    return @table[int(node) % 1000]\n'
-    file_text << '  end\n'
-    file_text << 'end\n\n'
+    file_text << "class HashTable\n"
+    file_text << "  def initialize()\n"
+    file_text << "    @table = Array.new(1000)\n"
+    file_text << "  end\n"
+    file_text << "  \n"
+    file_text << "  def add(new_node)\n"
+    file_text << "    table \n"
+    file_text << "    @table[int(new_node) % 1000] = newNode\n"
+    file_text << "  end\n"
+    file_text << "  def get(node)\n"
+    file_text << "    return @table[int(node) % 1000]\n"
+    file_text << "  end\n"
+    file_text << "end\n\n"
   end
+  # now write it to the file
+  ds_file.write(file_text)
+  ds_file.close
 end
